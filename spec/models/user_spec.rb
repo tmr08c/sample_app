b7f7379ff987edfa09a76d0a447a07bfc00af65b
Troy Rosenberg <jeremyaube@Jeremys-MacBook-Pro-3.local>
2012-05-16 13:59:22 -0400

    Completes Chapter 6's Exercises

--- spec/models/user_spec.rb
+++ spec/models/user_spec.rb
@@ -76,8 +76,6 @@ describe User do
   	end
   end
 
-  require File.dirname(__FILE__) + '/../spec_helper'
-  
   describe "when email address is already taken" do
     before do
       user_with_same_email = @user.dup
@@ -87,7 +85,16 @@ describe User do
 
     it { should_not be_valid }
   end
+ 
+  describe "email address with mixed case" do
+    let(:mixed_case_email) { "Foo@ExAMPle.CoM" }
 
+    it "should be saved as all lower-case" do
+      @user.email = mixed_case_email
+      @user.save
+      @user.reload.email.should == mixed_case_email.downcase
+    end
+  end
   # end EMAIL
 
   # PASSWORD
