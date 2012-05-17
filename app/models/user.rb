1a4e8ace1457d63b14d626c7480f447ee9a16c97
Troy Rosenberg <troy@roirevolution.com>
2012-05-17 15:47:18 -0400

    Completes Chapter 7 Exercises

--- app/models/user.rb
+++ app/models/user.rb
@@ -23,8 +23,7 @@ class User < ActiveRecord::Base
   									format: { with: VALID_EMAIL_REGEX },
 								   	uniqueness: { case_sensitive: false }
   
-  validates :password, 	presence: true, 
-   											length: {minimum: 6}
+  validates :password, 	length: {minimum: 6}
   
   validates :password_confirmation, presence: true
 
